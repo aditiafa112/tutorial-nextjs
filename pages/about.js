f4f3d79b8e1eeb44444126b2fe183202f95c37c1
Aditia <aditiafa112@gmail.com>
2022-02-21 09:01:50 +0700

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>Enim elit anim mollit ex enim sint consectetur pariatur.</p>
+    </div>
+  );
+};
+
+export default About;
